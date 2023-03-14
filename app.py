68bb0da72cdb08dbf3d10a106f12359dd0751534
Annie Lu <annielu@Anniesmndcheese.hsd1.wa.comcast.net>
2023-03-14 16:54:54 -0700

    Update .gitignore to include keys.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import functions
+import images
+import keys
 
 from flask import Flask, render_template, request
+from serpapi import GoogleSearch
 
 # Create an instance of Flask
 
@@ -18,10 +21,21 @@ def index():
 def results():
     if request.method == "POST":
         word = request.form["word"]
+        params = {
+        "q": word,
+        "tbm": "isch",
+        "ijn": "0",
+        "api_key": keys.MY_SECRET_API_KEY_1
+        }
+
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        images_results = results["images_results"]
         phonetics = functions.get_phonetics(word)
         partofspeech = functions.get_pos(word)
         definition = functions.get_definition(word)
+        image = images_results
         #return "Results that are close to {}".format(place)
-        return render_template("results.html", word=word, phonetics=phonetics, partofspeech=partofspeech, definition=definition)
+        return render_template("results.html", word=word, phonetics=phonetics, partofspeech=partofspeech, definition=definition, image=image)
     else:
         return "Wrong HTTP method", 400
--- images.py
+++ images.py
@@ -1,7 +1,7 @@
 from serpapi import GoogleSearch
 
 params = {
-  "q": "Apple",
+  "q": "word",
   "tbm": "isch",
   "ijn": "0",
   "api_key": "2c6fb04e9977afd291124be243194a28b0cfa1a1b6c960a98e8e1d08308787a6"
@@ -10,4 +10,5 @@ params = {
 search = GoogleSearch(params)
 results = search.get_dict()
 images_results = results["images_results"]
+print(images_results)
 #def display_results():
